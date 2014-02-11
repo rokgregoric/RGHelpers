d0ea3f64c5e4de95f24b04210bef785b8f45d325
Rok Gregorič <rok.gregoric@gmail.com>
2014-02-11 11:17:36 +0100

    Added RGCoreDataViewController - useful when using UIViewController instead if UITableViewController.

---
+++ CoreData/RGCoreDataViewController.m
@@ -0,0 +1,175 @@
+//
+//  CoreDataTableViewController.m
+//
+//  Created for Stanford CS193p Fall 2011.
+//  Copyright 2011 Stanford University. All rights reserved.
+//
+
+#import "RGCoreDataViewController.h"
+
+@interface RGCoreDataViewController()
+
+@property (nonatomic) BOOL beganUpdates;
+
+@end
+
+@implementation RGCoreDataViewController
+
+#pragma mark - Properties
+
+- (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation {
+    return YES;
+}
+
+- (void)setupDocument {
+    if (!self.document) {
+        [RGCoreDataManagedDocument.sharedDocument performWithDocument:^(UIManagedDocument *document) {
+            self.document = document;
+            [self setupFetchedResultsController];
+            [self fetchDataIntoDocument];
+        }];
+    }
+}
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    [self setupDocument];
+}
+
+#pragma mark - Fetching
+
+- (void)fetchDataIntoDocument {
+    NSAssert(NO, @"You should override this method when subclassing %@.", NSStringFromClass(self.superclass));
+}
+
+- (void)setupFetchedResultsController {
+    NSAssert(NO, @"You should override this method when subclassing %@.", NSStringFromClass(self.superclass));
+}
+
+- (void)performFetch {
+    if (self.fetchedResultsController) {
+        if (self.fetchedResultsController.fetchRequest.predicate) {
+            if (self.debug) NSLog(@"[%@ %@] fetching %@ with predicate: %@", NSStringFromClass(self.class), NSStringFromSelector(_cmd), self.fetchedResultsController.fetchRequest.entityName, self.fetchedResultsController.fetchRequest.predicate);
+        } else {
+            if (self.debug) NSLog(@"[%@ %@] fetching all %@ (i.e., no predicate)", NSStringFromClass(self.class), NSStringFromSelector(_cmd), self.fetchedResultsController.fetchRequest.entityName);
+        }
+        NSError *error;
+        [self.fetchedResultsController performFetch:&error];
+        if (error) NSLog(@"[%@ %@] %@ (%@)", NSStringFromClass(self.class), NSStringFromSelector(_cmd), error.localizedDescription, error.localizedFailureReason);
+    } else {
+        if (self.debug) NSLog(@"[%@ %@] no NSFetchedResultsController (yet?)", NSStringFromClass(self.class), NSStringFromSelector(_cmd));
+    }
+    [self.tableView reloadData];
+}
+
+- (void)setFetchedResultsController:(NSFetchedResultsController *)newfrc {
+    self.tableView.delegate = self;
+    self.tableView.dataSource = self;
+    NSFetchedResultsController *oldfrc = _fetchedResultsController;
+    if (newfrc != oldfrc) {
+        _fetchedResultsController = newfrc;
+        newfrc.delegate = self;
+        if ((!self.title || [self.title isEqualToString:oldfrc.fetchRequest.entity.name]) && (!self.navigationController || !self.navigationItem.title)) {
+            self.title = newfrc.fetchRequest.entity.name;
+        }
+        if (newfrc) {
+            if (self.debug) NSLog(@"[%@ %@] %@", NSStringFromClass(self.class), NSStringFromSelector(_cmd), oldfrc ? @"updated" : @"set");
+            [self performFetch];
+        } else {
+            if (self.debug) NSLog(@"[%@ %@] reset to nil", NSStringFromClass(self.class), NSStringFromSelector(_cmd));
+            [self.tableView reloadData];
+        }
+    }
+}
+
+#pragma mark - UITableViewDataSource
+
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView {
+    return self.fetchedResultsController.sections.count;
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+    return [self.fetchedResultsController.sections[section] numberOfObjects];
+}
+
+- (NSString *)tableView:(UITableView *)tableView titleForHeaderInSection:(NSInteger)section {
+    return [self.fetchedResultsController.sections[section] name];
+}
+
+- (NSInteger)tableView:(UITableView *)tableView sectionForSectionIndexTitle:(NSString *)title atIndex:(NSInteger)index {
+    return [self.fetchedResultsController sectionForSectionIndexTitle:title atIndex:index];
+}
+
+- (NSArray *)sectionIndexTitlesForTableView:(UITableView *)tableView {
+    return self.fetchedResultsController.sectionIndexTitles;
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    NSAssert(NO, @"You should override this method when subclassing %@.", NSStringFromClass(self.superclass));
+    return nil;
+}
+
+#pragma mark - NSFetchedResultsControllerDelegate
+
+- (void)controllerWillChangeContent:(NSFetchedResultsController *)controller {
+    if (!self.suspendAutomaticTrackingOfChanges) {
+        [self.tableView beginUpdates];
+        self.beganUpdates = YES;
+    }
+}
+
+- (void)controller:(NSFetchedResultsController *)controller didChangeSection:(id <NSFetchedResultsSectionInfo>)sectionInfo atIndex:(NSUInteger)sectionIndex forChangeType:(NSFetchedResultsChangeType)type {
+    if (!self.suspendAutomaticTrackingOfChanges) {
+        switch(type) {
+            case NSFetchedResultsChangeInsert:
+                [self.tableView insertSections:[NSIndexSet indexSetWithIndex:sectionIndex] withRowAnimation:UITableViewRowAnimationNone];
+                break;
+
+            case NSFetchedResultsChangeDelete:
+                [self.tableView deleteSections:[NSIndexSet indexSetWithIndex:sectionIndex] withRowAnimation:UITableViewRowAnimationNone];
+                break;
+        }
+    }
+}
+
+- (void)controller:(NSFetchedResultsController *)controller didChangeObject:(id)anObject atIndexPath:(NSIndexPath *)indexPath forChangeType:(NSFetchedResultsChangeType)type newIndexPath:(NSIndexPath *)newIndexPath {
+    if (!self.suspendAutomaticTrackingOfChanges) {
+        switch(type) {
+            case NSFetchedResultsChangeInsert:
+                [self.tableView insertRowsAtIndexPaths:@[newIndexPath] withRowAnimation:UITableViewRowAnimationNone];
+                break;
+
+            case NSFetchedResultsChangeDelete:
+                [self.tableView deleteRowsAtIndexPaths:@[indexPath] withRowAnimation:UITableViewRowAnimationNone];
+                break;
+
+            case NSFetchedResultsChangeUpdate:
+                [self.tableView reloadRowsAtIndexPaths:@[indexPath] withRowAnimation:UITableViewRowAnimationNone];
+                break;
+
+            case NSFetchedResultsChangeMove:
+                [self.tableView deleteRowsAtIndexPaths:@[indexPath] withRowAnimation:UITableViewRowAnimationNone];
+                [self.tableView insertRowsAtIndexPaths:@[newIndexPath] withRowAnimation:UITableViewRowAnimationNone];
+                break;
+        }
+    }
+}
+
+- (void)controllerDidChangeContent:(NSFetchedResultsController *)controller {
+    if (self.beganUpdates) {
+        [self.tableView endUpdates];
+    }
+}
+
+- (void)setSuspendAutomaticTrackingOfChanges:(BOOL)suspend {
+    if (suspend) {
+        _suspendAutomaticTrackingOfChanges = YES;
+    } else {
+        [NSOperationQueue.mainQueue addOperationWithBlock:^{
+            _suspendAutomaticTrackingOfChanges = NO;
+        }];
+    }
+}
+
+@end
